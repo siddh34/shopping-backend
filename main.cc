1b4276f73ec49566bad935b55ed5a21bf269ac80
siddh34 <ssiddharthsutar@gmail.com>
2024-07-06 23:50:37 +0530

    added working docker file

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@ int main() {
     drogon::app().addListener("0.0.0.0", 5555);
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
-    //drogon::app().loadConfigFile("../config.yaml");
+    // drogon::app().loadConfigFile("./config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
