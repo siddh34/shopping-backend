5ed346acebed93811bab5a2d1d782a19a40d2ed8
siddh34 <ssiddharthsutar@gmail.com>
2024-07-21 17:53:57 +0530

    added docker for sqllite

---
+++ utils/env.py
@@ -0,0 +1,5 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "Awesome API"
+    model_config = SettingsConfigDict(env_file=".env")
