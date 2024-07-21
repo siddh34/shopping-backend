d6965f878e0d5d2025802597d4274faef241f554
siddh34 <ssiddharthsutar@gmail.com>
2024-07-21 18:11:14 +0530

    added db

--- utils/env.py
+++ utils/env.py
@@ -3,3 +3,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
     model_config = SettingsConfigDict(env_file=".env")
+
