11cb416cf66dfcfab38ac3681421de588b090309
siddh34 <ssiddharthsutar@gmail.com>
2024-07-21 17:30:34 +0530

    done with fastapi setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"Server:": "Hi, I'm alive!"}
