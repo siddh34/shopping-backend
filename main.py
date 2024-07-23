3f77253ee8a62e5e53f473e59d5c65169ef1d067
siddh34 <ssiddharthsutar@gmail.com>
2024-07-23 22:32:36 +0530

    connected with db

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from .utils.env import Settings
+from sqlalchemy.orm import Session
+from .utils.database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.add_middleware(
@@ -10,6 +15,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+settings = Settings()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def read_root():
---
+++ utils/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = os.path.join(base_dir, "../database/database.db")
+absolute_database_url = f"sqlite:///{database_path}"
+
+SQLALCHEMY_DATABASE_URL = absolute_database_url
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
